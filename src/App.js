739ed8b0e24796cf8cc9120b523b3de601d01a97
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-06-22 14:15:46 +0545

    suggestion on console

--- src/App.js
+++ src/App.js
@@ -4,23 +4,33 @@ import axios from 'axios'
 function App() {
   const [user, setUser] = useState([])
   const [text, setText] = useState('')
+  const [suggestion, setsuggestion] = useState([])
   useEffect(() => {
    const loaduser=async () =>{
      const response = await axios.get('https://reqres.in/api/users?page=2')
-     console.log(response.data.data)
+    //  console.log(response.data.data)
      setUser(response.data.data)
    } 
    loaduser()
   }, [])
   
   const onchangeHandler=(text)=>{
+    let matches=[]
+    if (user.length>0) {
+      matches=user.filter(usr=>{
+        const refex=new RegExp(`${text}`,"gi"); 
+        return usr.email.match(refex)
+      })
+    }
+    console.log('match',matches)
+    setsuggestion(matches)
     setText(text)
   }
-  console.log(text)
+  // console.log(text)
   return (
     <div className="container">
-      <input className='col-md-12'type="text"onChange={(event)=>onchangeHandler(event.target.value)}></input>
-     
+      <input className='col-md-12' style={{marginTop: 20 }} type="text"onChange={(event)=>onchangeHandler(event.target.value)} value={text}></input>
+     <h1>{text}</h1>
     </div>
   );
 }
